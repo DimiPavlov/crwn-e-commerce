e183c1a9a12a8f5aa75410081540ebdd606fdcb3
Dimi <64038279+DimiPavlov@users.noreply.github.com>
2020-09-29 15:08:03 +0300

    small style changes

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,28 +1,26 @@
 import React from "react";
-import SHOP_DATA from "./ShopData.js";
+import SHOP_DATA from './ShopData.js';
 import Preview from "../components/Preview"
  
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-            this.state = {
-            collections: SHOP_DATA
-        };
-    }
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
 
-    render() {
-        const {collections} = this.state;
-        return (<div className="shop-page">
-
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <Preview key={id} {...otherCollectionProps} />
-            ))
-        }
-            
-        </div>);
-    }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <Preview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default ShopPage
+export default ShopPage;
